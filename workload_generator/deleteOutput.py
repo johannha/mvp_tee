2d31693c2f8695e9be3fda0fed4bf99d218b6f7f
Johann Friedrich Alexander Hartmann <j.hartmann.1@campus.tu-berlin.de>
2021-08-12 17:48:55 +0200

    Added event listener to bc

--- workload_generator/deleteOutput.py
+++ workload_generator/deleteOutput.py
@@ -5,3 +5,8 @@ dir = 'output'
 filelist = glob.glob(os.path.join(dir, "*"))
 for f in filelist:
     os.remove(f)
+
+tee = '../tee/output'
+teeList = glob.glob(os.path.join(tee, "*"))
+for x in teeList:
+    os.remove(x)
